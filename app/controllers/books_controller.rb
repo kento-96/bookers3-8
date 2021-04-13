e01a0c8744894c87c49ac6c29ac7c3ea5d72722b
kento-96 <kento.soccer.1107@gmail.com>
2021-04-13 23:33:19 +0000

    [Finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,36 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book =Book.new(book_params)
+    @books=Book.all
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
+    flash[:success] = 'Book was successfully created.'
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+     @book = Book.find(params[:id])
+     
+    if @book.update(book_params)
+       redirect_to book_path(@book)
+    else
+      render :edit
+    end
+    flash[:success] = 'Book was successfully  updated.'
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+   flash[:success] = 'Book was successfully deleted.'
   end
 
   private
